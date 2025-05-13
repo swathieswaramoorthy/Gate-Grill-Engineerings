3059ec02475e8ce19beecad5e3599e7275c5dacd
VAISHNAVI K <vaishnavikarthikeyan108@gmail.com>
2025-05-13 22:19:34 +0530

    payment

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const paymentSchema = new mongoose.Schema({
     razorpay_payment_id: {
@@ -41,5 +41,4 @@ const paymentSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 const Payment = mongoose.model('Payment', paymentSchema);
-
-module.exports = Payment;
+export default Payment;
